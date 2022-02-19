70b3908c2c284d2f15fb685a8db2cc5b993ffb71
sv720 <sv720@ic.ac.uk>
2022-02-19 18:19:03 +0000

    started on lexer and parser

---
+++ include/ast/ast_functions.hpp
@@ -0,0 +1,107 @@
+#ifndef ast_functions_hpp
+#define ast_functions_hpp
+
+#include "ast_expression.hpp"
+
+#include <cmath>
+
+class Function
+    : public Expression
+{
+private:
+    ExpressionPtr arg;
+protected:
+    Function(ExpressionPtr _arg)
+        : arg(_arg)
+    {}
+public:
+    virtual ~Function()
+    {
+        delete arg;
+    }
+
+    virtual const char * getFunction() const =0;
+
+    ExpressionPtr getArg() const
+    { return arg; }
+
+    virtual void print(std::ostream &dst) const override
+    {
+        dst<<getFunction()<<"( ";
+        arg->print(dst);
+        dst<<" )";
+    }
+
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const override
+    {
+        // NOTE : This should be implemented by the inheriting function nodes, e.g. LogFunction
+        double v = getArg()->evaluate(bindings); //traverse down args
+        return v;
+
+    }
+};
+
+class LogFunction
+    : public Function
+{
+public:
+    LogFunction(ExpressionPtr _arg)
+        : Function(_arg)
+    {}
+
+    virtual const char *getFunction() const
+    { return "log"; }
+
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const override
+    { 
+        double v = getArg()->evaluate(bindings); //traverse down args
+        return log(v);
+    }
+    // TODO-E : Override evaluate, and implement it
+};
+
+class ExpFunction
+    : public Function
+{
+public:
+    ExpFunction(ExpressionPtr _arg)
+        : Function(_arg)
+    {}
+
+    virtual const char *getFunction() const
+    { return "exp"; }
+
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const override
+    { 
+        double v = getArg()->evaluate(bindings); //traverse down args
+        return exp(v);
+    }
+};
+
+class SqrtFunction
+    : public Function
+{
+public:
+    SqrtFunction(ExpressionPtr _arg)
+        : Function(_arg)
+    {}
+
+    virtual const char *getFunction() const
+    { return "sqrt"; }
+
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const override
+    { 
+        double v = getArg()->evaluate(bindings); //traverse down args
+        return sqrt(v);
+    }
+};
+
+#endif
---
+++ src/maths_lexer.flex
@@ -0,0 +1,52 @@
+%option noyywrap
+
+%{
+// Avoid error "error: `fileno' was not declared in this scope"
+//extern "C" int fileno(FILE *stream);
+
+#include "maths_parser.tab.hpp"
+%}
+
+
+%%
+[*]             { return T_TIMES; }
+[/]             { return T_DIVIDE; }
+[+]             { return T_PLUS; }
+[\^]            { return T_EXPONENT; }
+[-]             { return T_MINUS; }
+
+[\[]            { return T_LSQBRACKET; } //TBC
+[\]]            { return T_RSQBRACKET; }
+[{]             { return T_LCURBRACKET; }
+[}]             { return T_RCULBRACKET; }
+[;]             { return T_SEMICOLON; }
+[:]             { return T_COLON; }
+[~]             { return T_TILDE; }
+[#]             { return T_HASH; }
+[!]             { return T_EXCLAMATIONMARK; }
+[?]             { return T_QUESTIONMARK; }
+
+
+[(]             { return T_LBRACKET; }
+[)]             { return T_RBRACKET; }
+
+return          { return T_RETURN; }
+while           { return T_WHILE; }
+for             { return T_FOR; }
+break           { return T_BREAK; }
+
+
+
+[0-9]+([.][0-9]*)? { yylval.number=strtod(yytext, 0); return T_NUMBER; }
+[a-z]+          { yylval.string=new std::string(yytext); return T_VARIABLE; }
+
+[ \t\r\n]+		{;}
+
+.               { fprintf(stderr, "Invalid token\n"); exit(1); }
+%%
+
+void yyerror (char const *s)
+{
+  fprintf (stderr, "Parse error : %s\n", s);
+  exit(1);
+}
